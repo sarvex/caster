70eea3f8e59ceaa90ef0119b4f8d8bc783d91e11
kimizhang <kaifan.zhang@ele.me>
2015-01-20 16:21:48 +0800

    detail changes

--- caster.py
+++ caster.py
@@ -7,9 +7,9 @@ import inspect
 app = Archer(__name__)
 zeus_client.read_config(ZEUS)
 
-def test_error_handler(meta,result):
+def test_error_handler(meta, result):
     print 'Exception occured'
-    if isinstance(result.error,zeus_client.constants['eus'].EUSUserException):
+    if isinstance(result.error, zeus_client.constants['eus'].EUSUserException):
         raise meta.app.thrift.EUSUserException(1, 'sdfsd', 'sdfdsf')
         
 
@@ -29,13 +29,13 @@ def ping():
     return zeus_client['eus'].ping()
 
 @app.api('update_region_and_its_group')
-def update_region_and_its_group(region_id,region,group_id):
-    return zeus_client['ers'].update_region_and_its_group(region_id,region,group_id)
+def update_region_and_its_group(region_id, region, group_id):
+    return zeus_client['ers'].update_region_and_its_group(region_id, region, group_id)
 
 @app.api('walle_update_mobile_and_name')
-def walle_update_mobile_and_name(user_id,mobile,name):
+def walle_update_mobile_and_name(user_id, mobile, name):
     return zeus_client['eus'].walle_update_mobile_and_name(
-            user_id,mobile,name)
+            user_id, mobile, name)
 
 @app.api('test_error_handler')
 def test_error():
